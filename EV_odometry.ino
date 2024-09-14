c8ff6e3792c9d83527d4234519286486e2980265
Lex Gu <lexgu314@gmail.com>
2024-09-13 22:14:15 -0400

    initial commit

---
+++ EV_odometry.ino
@@ -0,0 +1,62 @@
+#include <util/atomic.h> // For the ATOMIC_BLOCK macro
+
+#define ENCA 2 // YELLOW
+#define ENCB 13 // WHITE
+#define PWM 9
+#define IN2 5
+#define IN1 4
+#define buttonPin 3
+
+volatile int posi = 0; // specify posi as volatile: https://www.arduino.cc/reference/en/language/variables/variable-scope-qualifiers/volatile/
+volatile bool startLoop = false;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ENCA,INPUT);
+  pinMode(ENCB,INPUT);
+  attachInterrupt(digitalPinToInterrupt(ENCA),readEncoder,RISING);
+  
+  pinMode(PWM,OUTPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+
+  pinMode(buttonPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(3), buttonPressed, FALLING);
+  
+}
+
+void loop() {
+  Serial.println(posi);
+
+  
+}
+
+void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
+  analogWrite(pwm,pwmVal);
+  if(dir == 1){
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+  }
+  else if(dir == -1){
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
+  }
+  else{
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,LOW);
+  }  
+}
+
+void readEncoder(){
+  int b = digitalRead(ENCB);
+  if(b > 0){
+    posi++;
+  }
+  else{
+    posi--;
+  }
+}
+
+void buttonPressed() {
+  posi = 0;
+}
